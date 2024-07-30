aa090368db7c63f773b828a863bed51444769234
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-07-30 21:32:43 +0530

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./component/Header";
 import Horizontal from "./component/Horizontal";
+import Slider from "./component/Slider";
 
 export default function Home() {
   return (
     <div>
       <Header/>
       {/* <Horizontal/> */}
+      <Slider/>
     </div>
   );
 }
