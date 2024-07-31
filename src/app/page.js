88dd9ed6c55cbbc6053f69486e1f981c4ceaf45a
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-07-31 10:52:05 +0530

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import Horizontal from "./component/Horizontal";
 import Slider from "./component/Slider";
 import Footer from "./component/Footer";
 import Circle from "./component/Circle";
+import Joining from "./component/Joining";
 
 export default function Home() {
   return (
     <div>
       <Header/>
       {/* <Horizontal/> */}
+      <Joining/>
       <Slider/>
       <Circle/>
       <Footer/>
