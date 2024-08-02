26d1b2caf226539608d7c03b605376dcf8f3185d
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-08-02 15:11:35 +0530

    First commit

--- src/app/component/Section1.jsx
+++ src/app/component/Section1.jsx
@@ -12,8 +12,8 @@ export default function Section1() {
                                     <div class="cta-text">Join waitlist</div>
                                     <div class="cta-text">Join waitlist</div>
                                 </div>
-                                <img class="w-[.6rem] ml-[.2rem] arrow-right" width="24" height="23" src='./arrow.svg' />
-                                <img class="absolute top-0 left-[50%] translate-x-[-50%] w-[6.8rem] z-[-1] mobile:w-[8rem] mobile:mt-[-.5rem] mobile:hidden" width="268" height="44" src="./color.svg" />
+                                <img class="w-[.6rem] ml-[.2rem] arrow-right" width="24" height="23" src='./Buenro-7.svg' />
+                                <img class="absolute top-0 left-[50%] translate-x-[-50%] w-[6.8rem] z-[-1] mobile:w-[8rem] mobile:mt-[-.5rem] mobile:hidden" width="268" height="44" src="./Buenro-8.svg" />
                                 <img class="absolute hidden top-0 left-[50%] translate-x-[-50%] w-[6.8rem] z-[-1] mobile:w-[9rem] mobile:mt-[-.1rem] mobile:inline-block" width="197" height="40" src='./empty.svg' />
                             </div>
                             <div class="absolute top-[-10%] left-[-13%] right-[-13%] bottom-[-13%]"></div>
@@ -24,8 +24,8 @@ export default function Section1() {
                                     <div class="cta-text">Join waitlist</div>
                                     <div class="cta-text">Join waitlist</div>
                                 </div>
-                                <img class="w-[.6rem] ml-[.2rem] arrow-right" width="24" height="23" src='./arrow.svg' />
-                                <img class="absolute top-0 left-[50%] translate-x-[-50%] w-[6.8rem] z-[-1] mobile:w-[8rem] mobile:mt-[-.5rem] mobile:hidden" width="268" height="44" src="./color.svg" />
+                                <img class="w-[.6rem] ml-[.2rem] arrow-right" width="24" height="23" src='./Buenro-7.svg' />
+                                <img class="absolute top-0 left-[50%] translate-x-[-50%] w-[6.8rem] z-[-1] mobile:w-[8rem] mobile:mt-[-.5rem] mobile:hidden" width="268" height="44" src="./Buenro-8.svg" />
                                 <img class="absolute hidden top-0 left-[50%] translate-x-[-50%] w-[6.8rem] z-[-1] mobile:w-[9rem] mobile:mt-[-.1rem] mobile:inline-block" width="197" height="40" src='./empty.svg' />
                             </div>
                             <div class="absolute top-[-10%] left-[-13%] right-[-13%] bottom-[-13%]"></div>
@@ -48,22 +48,22 @@ export default function Section1() {
             <div className='h-[calc(16rem+17vw)] mobile:w-[100vw] mobile:relative mobile:h-[28rem] mobile:overflow-hidden mobile:mt-[1rem]'>
                 <div className='absolute flex flex-row overflow-hidden srow-container w-[48rem] min-w-[100vw] left-[50%] translate-x-[-50%] mobile:w-[68rem]'>
                     <div className='flex flex-shrink-0 gap-[3vw] pl-[1.6rem] sync-row-contain md:gap-[1.6rem]'>
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]"  />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]"  />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]"  />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]"  />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]"  />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]"  />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]"  />
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]" srcSet='./mobile/1.webp 1x, ./mobile/1.webp 2x' src='./mobile/1.webp'/>
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]" srcSet='./mobile/2.webp 1x, ./mobile/2.webp 2x' src='./mobile/2.webp'/>
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]" srcSet='./mobile/3.webp 1x, ./mobile/3.webp 2x' src='./mobile/3.webp' />
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]" srcSet='./mobile/6.webp 1x, ./mobile/6.webp 2x' src='./mobile/6.webp' />
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]" srcSet='./mobile/9.webp 1x, ./mobile/9.webp 2x' src='./mobile/9.webp' />
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]"  srcSet='./mobile/12.webp 1x, ./mobile/12.webp 2x' src='./mobile/12.webp'/>
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem]  min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] mobile:border-[.12rem] mobile:rounded-[1.6rem]"  srcSet='./mobile/17.webp 1x, ./mobile/17.webp 2x' src='./mobile/17.webp'/>
                     </div>
                         <div className='flex flex-shrink-0 gap-[3vw] pl-[1.6rem] sync-row-contain md:gap-[1.6rem]'>
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " />
-                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " />
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " srcSet='./mobile/1.webp 1x, ./mobile/1.webp 2x' src='./mobile/1.webp'/>
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " srcSet='./mobile/2.webp 1x, ./mobile/2.webp 2x' src='./mobile/2.webp' />
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " srcSet='./mobile/3.webp 1x, ./mobile/3.webp 2x' src='./mobile/3.webp' />
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " srcSet='./mobile/6.webp 1x, ./mobile/6.webp 2x' src='./mobile/6.webp'/>
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " srcSet='./mobile/9.webp 1x, ./mobile/9.webp 2x' src='./mobile/9.webp'/>
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " srcSet='./mobile/12.webp 1x, ./mobile/12.webp 2x' src='./mobile/12.webp'/>
+                        <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[7.8rem] min-w-[16.66vw] opacity-[.3] h-auto bg-black rounded-[1.1rem] border-[.08rem] border-[#F2F2F2] mobile:w-[11.8rem] " srcSet='./mobile/17.webp 1x, ./mobile/17.webp 2x' src='./mobile/17.webp'/>
                         </div>
                     </div>
                 </div>
