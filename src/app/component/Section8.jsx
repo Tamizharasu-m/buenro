5d2fe78481a754741fe5378bf3cf56e596f2017d
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-08-04 09:55:47 +0530

    First commit

--- src/app/component/Section8.jsx
+++ src/app/component/Section8.jsx
@@ -3,31 +3,31 @@ import React from 'react'
 export default function Section8() {
     return (
         <div className='relative text-center text-black bg-white-100'>
-            <div className='sticky top-0 w-full h-screen md:hidden'>
+            <div className='sticky top-0 hidden w-full h-screen md:flex'>
                 <div className='absolute w-full h-full top-[0] overflow-hidden'>
                     <img alt="" loading="lazy" width="645" height="1400" decoding="async" data-nimg="1" class="slides-Image left shadow-xxsm object-cover absolute top-[50%] left-[calc(.55rem+10vw)] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] rotate-[-10deg] md:left-[1rem]" srcSet='./trust/18.jpg 1x, ./trust/18.jpg 2x' src='./trust/18.jpg' />
                     <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="slides-Image left object-cover shadow-xxsm absolute top-[50%] left-[calc(.55rem+10vw)] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] rotate-[10deg]" srcSet='./trust/2.jpg 1x, ./trust/2.jpg 2x' src='./trust/2.jpg' />
                 </div>
                 <div className='absolute w-full h-full top-[0] overflow-hidden'>
-                <img alt="" loading="lazy" width="633" height="1400" decoding="async" data-nimg="1" class="slides-Image right object-cover absolute top-[50%] right-[calc(.55rem+10vw)] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] rotate-[10deg]" srcSet='./trust/19.jpg 1x, ./trust/19.jpg 2x' src='./trust/19.jpg' />
-                <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="slides-Image right object-cover absolute top-[50%] right-[calc(.55rem+10vw)] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] rotate-[-10deg]" srcSet='./trust/20.jpg 1x, ./trust/20.jpg 2x' src='./trust/20.jpg' />
+                    <img alt="" loading="lazy" width="633" height="1400" decoding="async" data-nimg="1" class="slides-Image right object-cover absolute top-[50%] right-[calc(.55rem+10vw)] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] rotate-[10deg]" srcSet='./trust/19.jpg 1x, ./trust/19.jpg 2x' src='./trust/19.jpg' />
+                    <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="slides-Image right object-cover absolute top-[50%] right-[calc(.55rem+10vw)] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] rotate-[-10deg]" srcSet='./trust/20.jpg 1x, ./trust/20.jpg 2x' src='./trust/20.jpg' />
                 </div>
             </div>
             <div className='mx-auto slides-part w-[24ch] mt-[-100vh] min-h-[100vh] flex flex-col justify-center gap-[.9rem] pt-[2.9rem] pb-[3.1rem] md:min-h-[13rem] md:mt-[0] md:justify-between md:w-[52%] md:text-[1.1rem] mobile:w-[85%] mobile:gap-[2rem] mobile:text-[1.26rem] mobile:pb-0'>
-            <h5 class="text-[1.9em] leading-[1.5] mobile:text-[1.75rem]">Connect with <span class="text-[#999999]">Trusted</span> Circles</h5>
-            <p class="text-regular30 mobile:text-[.759em] mobile:text-[.87rem] mobile:max-w-[34ch] mobile:w-auto mobile:mx-auto">Transform how you connect with your trusted social circles - from private members clubs, to leadership networks, conferences, and festivals.</p>
+                <h5 class="text-[1.9em] leading-[1.5] mobile:text-[1.75rem]">Connect with <span class="text-[#999999]">Trusted</span> Circles</h5>
+                <p class="text-regular30 mobile:text-[.759em] mobile:text-[.87rem] mobile:max-w-[34ch] mobile:w-auto mobile:mx-auto">Transform how you connect with your trusted social circles - from private members clubs, to leadership networks, conferences, and festivals.</p>
             </div>
-            <div className='hidden w-full mobile:pt-[3.1rem] overflow-hidden mobile-pinning-slides md:flex pb-[5rem] flex-row justify-center gap-[1.4rem] mobile:px-[1.2rem] mobile:justify-between mobile:w-full mobile:gap-[.3rem] '>
-            <img alt="" loading="lazy" width="645" height="1400" decoding="async" data-nimg="1" class="slides-Image  shadow-xxsm object-cover top-[4.2rem] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] md:left-[1rem] mobile:min-h-[17rem] mobile:h-[auto] mobile:w-[47%]" srcSet='./trust/18.jpg 1x, ./trust/18.jpg 2x' src='./trust/18.jpg' />
-            <img alt="" loading="lazy" width="633" height="1400" decoding="async" data-nimg="1" class="slides-Image  object-cover top-[4.2rem] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] md:left-[1rem] mobile:min-h-[17rem] mobile:h-[auto] mobile:w-[47%]" srcSet='./trust/2.jpg 1x, ./trust/2.jpg 2x' src='./trust/2.jpg'/>
+            <div className='flex w-full mobile:pt-[3.1rem] overflow-hidden mobile-pinning-slides  pb-[5rem] flex-row justify-center gap-[1.4rem] mobile:px-[1.2rem] mobile:justify-between mobile:w-full mobile:gap-[.3rem] md:hidden'>
+                <img alt="" loading="lazy" width="645" height="1400" decoding="async" data-nimg="1" class="slides-Image  shadow-xxsm object-cover top-[4.2rem] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] md:left-[1rem] mobile:min-h-[17rem] mobile:h-[auto] mobile:w-[47%]" srcSet='./trust/18.jpg 1x, ./trust/18.jpg 2x' src='./trust/18.jpg' />
+                <img alt="" loading="lazy" width="633" height="1400" decoding="async" data-nimg="1" class="slides-Image  object-cover top-[4.2rem] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] md:left-[1rem] mobile:min-h-[17rem] mobile:h-[auto] mobile:w-[47%]" srcSet='./trust/2.jpg 1x, ./trust/2.jpg 2x' src='./trust/2.jpg' />
             </div>
             <div className='mx-auto slides-part w-[25ch] min-h-[100vh] flex flex-col justify-center gap-[.9rem] pt-[2.9em] pb-[3.2em] md:min-h-[13rem] md:justify-between md:w-[52%] md:text-[1.1rem] mobile:pb-[1.3rem] mobile:w-[85%] mobile:gap-[2rem] mobile:text-[1.26rem] mobile:pb-0'>
-            <h5 class="text-[1.9em] leading-[1.5] mobile:text-[1.75rem]">Join Exclusive Trips <span class="text-[#999999]">&amp;</span> Events</h5>
-            <p class="text-regular30 mobile:text-[.759em] mobile:text-[.87rem] mobile:max-w-[34ch] mobile:w-auto mobile:mx-auto"><span class="text-[#999999]">Whether it's a local gathering, an exciting trip, or a stay at a unique property,</span> easily find and become part of events and groups that match your interests.</p>
+                <h5 class="text-[1.9em] leading-[1.5] mobile:text-[1.75rem]">Join Exclusive Trips <span class="text-[#999999]">&amp;</span> Events</h5>
+                <p class="text-regular30 mobile:text-[.759em] mobile:text-[.87rem] mobile:max-w-[34ch] mobile:w-auto mobile:mx-auto"><span class="text-[#999999]">Whether it's a local gathering, an exciting trip, or a stay at a unique property,</span> easily find and become part of events and groups that match your interests.</p>
             </div>
-            <div className='hidden w-full overflow-hidden mobile-pinning-slides md:flex pb-[5rem] flex-row  justify-center gap-[1.4rem] mobile:px-[1.2rem] mobile:justify-between mobile:w-full mobile:gap-[.3rem] mobile:pt-[3rem] '>
-            <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="slides-Image  shadow-xxsm object-cover top-[4.2rem] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] md:left-[1rem] mobile:w-[47%] mobile:min-h-[17rem] mobile:h-[auto]" srcSet='./trust/19.jpg 1x, ./trust/19.jpg 2x' src='./trust/19.jpg' />
-            <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="slides-Image  object-cover top-[4.2rem] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] md:left-[1rem] mobile:w-[47%] mobile:min-h-[17rem] mobile:h-[auto]" srcSet='./trust/20.jpg 1x, ./trust/20.jpg 2x' src='./trust/20.jpg' />
+            <div className='flex w-full overflow-hidden mobile-pinning-slides md:hidden pb-[5rem] flex-row  justify-center gap-[1.4rem] mobile:px-[1.2rem] mobile:justify-between mobile:w-full mobile:gap-[.3rem] mobile:pt-[3rem] '>
+                <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="slides-Image  shadow-xxsm object-cover top-[4.2rem] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] md:left-[1rem] mobile:w-[47%] mobile:min-h-[17rem] mobile:h-[auto]" srcSet='./trust/19.jpg 1x, ./trust/19.jpg 2x' src='./trust/19.jpg' />
+                <img alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="slides-Image  object-cover top-[4.2rem] w-[8.16rem] border-[.09rem] border-black h-[17.75rem] rounded-[1.2rem] md:left-[1rem] mobile:w-[47%] mobile:min-h-[17rem] mobile:h-[auto]" srcSet='./trust/20.jpg 1x, ./trust/20.jpg 2x' src='./trust/20.jpg' />
             </div>
         </div>
     )
