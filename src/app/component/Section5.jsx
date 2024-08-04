006c13982351bd988211412f60f61551fc4353f5
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-08-04 18:07:38 +0530

    First commit

--- src/app/component/Section5.jsx
+++ src/app/component/Section5.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Section5() {
   return (
-    <div className='flex flex-col members-container overflow-container gap-[.5rem] text-center bg-white-100 text-black pt-[2.5rem] mobile:pb-[3.5rem]'>
+    <div className='flex flex-col members-container overflow-container gap-[.5rem] text-center bg-white-100 text-black pt-[2.5rem] mobile:pb-[3.5rem] '>
         <div>
-            <h3 className='text-regular75 w-[20ch] mx-auto mobile:text-[]2.1rem mobile:w-full mobile:px-[1.2rem] mobile:leading-[1.5]'>See who else is joining</h3>
+            <h3 className='text-regular75 w-[20ch] mx-auto mobile:text-[2.1rem] mobile:w-full mobile:px-[1.2rem] mobile:leading-[1.5]'>See who else is joining</h3>
         </div>
         <div>
             <div className='pt-[.94rem] pb-[4.rem] flex flex-row justify-center members-cards mobile:pt-[3rem]'>
