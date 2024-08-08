dba77c44a8e463b7649783a93e7867877f806a97
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-08-08 15:14:32 +0530

    First commit

--- src/app/component/Circle/Circle.jsx
+++ src/app/component/Circle/Circle.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function Circle() {
   return (
     <div className='w-full h-full overflow-hidden '>
       <section className='h-screen bg-white-100 relative md:mt-[1.7rem] mt-[8rem]'>
         <div className='flex items-center justify-center h-screen'>
-          <p className='text-[2.78rem] w-[8ch] mx-auto text-center absolute'>Join Circles</p>
+          <p className='text-[2.78rem] w-[8ch] mx-auto text-center absolute lg:text-[76px]'>Join Circles</p>
           <div className='absolute h-[calc(33vh+16rem)] w-[calc(33vh+16rem)] top-0 flex items-center justify-center circles-container mobile:w-full mobile:h-[115vw]'>
             <div className='absolute top emoji top-[2.5%] w-[5.47rem] h-[5.47rem] rounded-full bg-whit-100 border-[.05rem] border-white-900 shadow-xsm mobile:w-[4.8rem] mobile:h-[4.8rem]'>
               <img loading='lazy' width="868" height="864" decoding='async' data-nimg="1" className='rounded-full' srcSet='./face/1.webp 1x, ./face/1.webp 2x' src="./face/1.webp" alt="" />
--- src/app/component/Slider/Slider.jsx
+++ src/app/component/Slider/Slider.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client"
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React from 'react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Slider() {
+
+    useEffect(() => {
+        gsap.set('#left-slider', { x: 0, duration: 1 }, 0)
+        gsap.set('#right-slider', { x: -500, duration: 1 }, 0)
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.three-slide',
+                start: 'top 100%',
+                end: 'bottom 40%',
+                scrub: true,
+                markers: true,
+            }
+        });
+
+        tl.to('#left-slider', { x: 500, duration: 1 }, 0)
+            .to('#right-slider', { x: 0, duration: 1 }, 0);
+
+        return () => {
+            if (tl.scrollTrigger) tl.scrollTrigger.kill();
+            tl.kill();
+        };
+    }, []);
+
+
     return (
-        <div className='h-screen md:h-[165vh] text-center mobile:mb-[-16rem] overflow-hidden '>
-            <div className='flex items-center h-full gap-[2.35rem] screens-showcase rotate-[-10deg]' >
+        <div className='h-screen md:h-[165vh] text-center mobile:mb-[-16rem] overflow-hidden three-slide'>
+            <div id='right-slider' className='flex items-center h-full gap-[2.35rem] screens-showcase rotate-[-10deg]' >
                 <img loading='lazy' width="644" height="1400" decoding='async' data-nimg='1' className="md:w-[8.16rem] md:h-[17.75rem] bg-black rounded-[1.1rem] border-[.09rem] border-black w-auto h-[25.75rem]" srcSet='./mobile/1.webp 1x ,./mobile/1.webp 2x ' src="./mobile/1.webp" alt="" />
                 <img loading='lazy' width="644" height="1400" decoding='async' data-nimg='1' className="md:w-[8.16rem] md:h-[17.75rem] bg-black rounded-[1.1rem] border-[.09rem] border-black w-auto h-[25.75rem]" srcSet='./mobile/2.webp 1x ,./mobile/2.webp 2x ' src="./mobile/2.webp" alt="" />
                 <img loading='lazy' width="644" height="1400" decoding='async' data-nimg='1' className="md:w-[8.16rem] md:h-[17.75rem] bg-black rounded-[1.1rem] border-[.09rem] border-black w-auto h-[25.75rem]" srcSet='./mobile/3.webp 1x ,./mobile/3.webp 2x ' src="./mobile/3.webp" alt="" />
