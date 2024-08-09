0ea8b68311624897c4e3f06939c4f9a227321e80
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-08-09 14:33:37 +0530

    First commit

--- src/app/component/Slider/Slider.jsx
+++ src/app/component/Slider/Slider.jsx
@@ -33,7 +33,7 @@ export default function Slider() {
 
     return (
         <div className='h-screen md:h-[165vh] text-center mobile:mb-[-16rem] overflow-hidden three-slide'>
-            <div id='right-slider' className='flex items-center h-full gap-[2.35rem] screens-showcase rotate-[-10deg]' >
+            <div id='right-slider' className='flex items-center h-full gap-[2.35rem]  rotate-[-10deg]' >
                 <img loading='lazy' width="644" height="1400" decoding='async' data-nimg='1' className="md:w-[8.16rem] md:h-[17.75rem] bg-black rounded-[1.1rem] border-[.09rem] border-black w-auto h-[25.75rem]" srcSet='./mobile/1.webp 1x ,./mobile/1.webp 2x ' src="./mobile/1.webp" alt="" />
                 <img loading='lazy' width="644" height="1400" decoding='async' data-nimg='1' className="md:w-[8.16rem] md:h-[17.75rem] bg-black rounded-[1.1rem] border-[.09rem] border-black w-auto h-[25.75rem]" srcSet='./mobile/2.webp 1x ,./mobile/2.webp 2x ' src="./mobile/2.webp" alt="" />
                 <img loading='lazy' width="644" height="1400" decoding='async' data-nimg='1' className="md:w-[8.16rem] md:h-[17.75rem] bg-black rounded-[1.1rem] border-[.09rem] border-black w-auto h-[25.75rem]" srcSet='./mobile/3.webp 1x ,./mobile/3.webp 2x ' src="./mobile/3.webp" alt="" />
