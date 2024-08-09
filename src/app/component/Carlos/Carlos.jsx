b5190c433d28742de3321aee03e1975638298fef
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-08-09 14:37:29 +0530

    First commit

--- src/app/component/Carlos/Carlos.jsx
+++ src/app/component/Carlos/Carlos.jsx
@@ -32,7 +32,7 @@ export default function Carlos() {
 
     
   return (
-    <div className=' h-screen items-center justify-center flex flex-col members-container overflow-hidden gap-[.5rem] text-center bg-white-100 text-black pt-[2.5rem] pb-[3.5rem] md:pb-[0] '>
+    <div className=' h-screen items-center justify-center flex flex-col  overflow-hidden gap-[.5rem] text-center bg-white-100 text-black pt-[2.5rem] pb-[3.5rem] md:pb-[0] '>
         <div>
             <h3 className='text-[1.9rem] md:w-[20ch] mx-auto  w-full px-[1.2rem] md:px-0 leading-[1.5] md:leading-[0]'>See who else is joining</h3>
         </div>
@@ -45,12 +45,12 @@ export default function Carlos() {
                 </div>
                 <div className='z-10 shrink-0 '>
                     <img loading='lazy' width="650" height="650" decoding='async' data-nimg="1" className='w-[8.9rem] h-[8.9rem] mx-auto rounded-full mb-[.87rem] object-cover border-[.4rem] border-white  md:w-[9.8rem] md:h-[9.8rem] mt-[-.9rem] md:mt-0'  srcSet='./new.jpg 16w, ./new.jpg 32w ./new.jpg 48w, ./new.jpg 64w, ./new.jpg 96w, ./new.jpg 128w, ./new.jpg 256w, ./new.jpg 384w, ./new.jpg 640w, ./new.jpg 750w, ./new.jpg 828w, ./new.jpg 1080w, ./new.jpg 1200w, ./new.jpg 1920w, ./new.jpg 2048w, ./new.jpg 3840w,' src="./new.jpg" alt="" />
-                    <p className=' mt-[1.8rem] md:mt-[0] text-[.9rem]'>"Carlos M." <img loading='lazy' width="160" height="160" decoding='async' data-nimg="1" className='w-[.7rem] inline-block mobile:w-[.9rem]' srcSet='./us.webp 1x, ./us.webp 2x' src="" alt="./us.webp" /></p>
+                    <p className=' mt-[1.8rem] md:mt-[0] text-[.9rem]'>"Carlos M." <img loading='lazy' width="160" height="160" decoding='async' data-nimg="1" className='w-[.7rem] inline-block ' srcSet='./us.webp 1x, ./us.webp 2x' src="" alt="./us.webp" /></p>
                     <p className='text-[.556rem] text-gray-500 leading-[1.3] '>Remote Designer</p>
                 </div>
                 <div id='right-carlos' className='shrink-0 '>
                 <img loading='lazy' width="1500" height="1000" decoding='async' data-nimg="1" className='w-[8.1rem] h-[8.1rem] mx-auto rounded-full mb-[.87rem] object-cover md:w-[8rem] md:h-[8rem]'  srcSet='./person1.webp 16w, ./person1.webp 32w ./person1.webp 48w, ./person1.webp 64w, ./person1.webp 96w, ./person1.webp 128w, ./person1.webp 256w, ./person1.webp 384w, ./person1.webp 640w, ./person1.webp 750w, ./person1.webp 828w, ./person1.webp 1080w, ./person1.webp 1200w, ./person1.webp 1920w, ./person1.webp 2048w, ./person1.webp 3840w,' src="./person1.webp" alt="" />
-                    <p className='mt-[1.8rem] md:mt-[0] text-[.9rem]'>"Emily S." <img loading='lazy' width="160" height="160" decoding='async' data-nimg="1" className='w-[.7rem] inline-block mobile:w-[.9rem]' srcSet='./ca.webp 1x, ./ca.webp 2x' src="" alt="./ca.webp" /></p>
+                    <p className='mt-[1.8rem] md:mt-[0] text-[.9rem]'>"Emily S." <img loading='lazy' width="160" height="160" decoding='async' data-nimg="1" className='w-[.7rem] inline-block ' srcSet='./ca.webp 1x, ./ca.webp 2x' src="" alt="./ca.webp" /></p>
                     <p className='text-[.556rem] text-gray-500 leading-[1.3] '>Solo Traveler</p>
                 </div>
             </div>
