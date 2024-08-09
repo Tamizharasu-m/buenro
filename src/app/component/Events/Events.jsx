3de916e985f4951b43d43e29aaf085023059af13
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-08-09 08:30:13 +0530

    First commit

--- src/app/component/Events/Events.jsx
+++ src/app/component/Events/Events.jsx
@@ -44,7 +44,7 @@ export default function Events() {
                     <img id='bottom-slider' alt="" loading="lazy" width="644" height="1400" decoding="async" data-nimg="1" class="w-[8.3em] rounded-[1em] h-[auto] border-[.06em] border-black mobile:text-[1.5rem]" srcSet='./section11/14.webp 1x, ./section11/14.webp 2x' src='./section11/14.webp' />
                 </div>
             </div>
-            <div id='top-slider' className='flex flex-col sc-col gap-[2rem] pt-[1.6rem] md:pt-0 mt-[20px] lg:mt-[0]'>
+            <div id='top-slider' className='flex flex-col sc-col gap-[2rem] pt-[1.6rem] md:pt-0 mt-[100px] lg:mt-[0]'>
                 <div className='h-[16rem] overflow-hidden bg-violet-grad px-[1.2rem] capitalize gap-[1rem] pt-[1.6rem] text-center w-[15.22rem] rounded-[1.3rem] items-center border-[.09rem] border-black flex flex-col mobile:w-full mobile:pt-[2.2rem] mobile:border-[.16rem] mobile:px-[1rem] mobile:gap-[1.4rem] mobile:rounded-[2.2rem] mobile:h-[23rem]'>
                 <p class="text-regular45 leading-[1.5] mb-[-.3rem] mobile:text-[1.75rem]">Find <span class="text-[#808080]">your</span>People</p>
                 <p class="text-sm20 mobile:text-[.91rem]">Meet new friends <span class="text-[#808080]">all over the world.</span></p>
